db5af7e8d18ada62b1b31524eddf819918793c7e
Emiel Kollof <emiel@kollof.nl>
2026-01-16 12:44:06 +0100

    feat: re-enable XEP-0198 Stream Management
    Re-enabled our SM implementation now that libstrophe's built-in
    SM is disabled via XMPP_CONN_FLAG_DISABLE_SM flag. This prevents
    conflicts between the two implementations.
    
    Our implementation:
    - Sends <enable/> on connection
    - Handles <enabled/>, <failed/>, <a/>, <r/>
    - Tracks h_inbound/h_outbound counters
    - Periodic ack requests (30s timer)
    - Graceful failure handling
    
    Note: Session resumption not yet implemented.

--- connection.cpp
+++ connection.cpp
@@ -1902,15 +1902,16 @@ bool weechat::connection::conn_handler(event status, int error, xmpp_stream_erro
         account.idle_timer_hook = weechat_hook_timer(60 * 1000, 0, 0,
                                                      &account::idle_timer_cb, &account, nullptr);
 
-        // TEMPORARY: Disable SM completely to test if it's causing disconnects
         // Enable Stream Management (XEP-0198) if available
+        // Note: libstrophe's built-in SM is disabled via XMPP_CONN_FLAG_DISABLE_SM
+        // so we're using our own implementation
         // Only try once per manual connect - don't retry on auto-reconnect if failed
-        // if (account.sm_available)
-        // {
-        //     this->send(stanza::xep0198::enable(true, 300)
-        //                .build(account.context)
-        //                .get());
-        // }
+        if (account.sm_available)
+        {
+            this->send(stanza::xep0198::enable(true, 300)
+                       .build(account.context)
+                       .get());
+        }
 
         (void) weechat_hook_signal_send("xmpp_account_connected",
                                         WEECHAT_HOOK_SIGNAL_STRING, account.name.data());
