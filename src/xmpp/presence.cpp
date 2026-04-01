4aeec865f1b28a1e63b2f9e88ec79eb76d6ffc30
Emiel Kollof <emiel@kollof.nl>
2026-04-01 16:23:48 +0200

    refactor(phase4): remove dead stanza__* legacy C declarations and definitions

--- src/xmpp/presence.cpp
+++ src/xmpp/presence.cpp
@@ -1,39 +1,3 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, version 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-#include <cstdlib>
-#include <strophe.h>
-
-#include "stanza.hh"
-
-xmpp_stanza_t *stanza__presence(xmpp_ctx_t *context, xmpp_stanza_t *base,
-                                xmpp_stanza_t **children, const char *ns,
-                                const char *from, const char *to, const char *type)
-{
-    xmpp_stanza_t *parent = base ? base : xmpp_presence_new(context);
-    xmpp_stanza_t **child = children;
-
-    if (ns)
-        xmpp_stanza_set_ns(parent, ns);
-
-    if (from)
-        xmpp_stanza_set_from(parent, from);
-
-    if (to)
-        xmpp_stanza_set_to(parent, to);
-
-    if (type)
-        xmpp_stanza_set_attribute(parent, "type", type);
-
-    while (*child)
-    {
-        xmpp_stanza_add_child(parent, *child);
-        xmpp_stanza_release(*child);
-
-        ++child;
-    }
-
-    return parent;
-}
-
